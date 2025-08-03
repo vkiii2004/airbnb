61034aef232fdc409a36b88241a93ffee72f2ced
vishal pandhare <vpandhare874@gmail.com>
2025-08-03 17:04:46 +0530

    add

--- app.js
+++ app.js
@@ -111,9 +111,9 @@ app.use("/listings",listingsRouter );
 app.use("/listings/:id/reviews",reviewsRouter );
 app.use("/",userRouter );
 
-app.all("*", (req, res, next) => {
-    next(new ExpressError(404, "Page Not Found!"));
-});
+// app.all("*", (req, res, next) => {
+//     next(new ExpressError(404, "Page Not Found!"));
+// });
 
 
 app.use((err, req, res, next) => {
